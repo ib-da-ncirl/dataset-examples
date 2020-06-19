6624792000ed2604b7a06efd2e0c9d08c3559a55
Ian Buttimer <x14112451@student.ncirl.ie>
2020-06-19 19:04:04 +0100

    Updated json_to_csv_converter for python 3

--- json_to_csv_converter.py
+++ json_to_csv_converter.py
@@ -9,16 +9,23 @@ import collections
 import csv
 import simplejson as json
 
+try:
+    collectionsAbc = collections.abc
+except AttributeError:
+    collectionsAbc = collections
+
 
 def read_and_write_file(json_file_path, csv_file_path, column_names):
     """Read in the json dataset file and write it out to a csv file, given the column names."""
-    with open(csv_file_path, 'wb+') as fout:
+    with open(csv_file_path, 'w+') as fout:
+    # with open(csv_file_path, 'wb+') as fout:
         csv_file = csv.writer(fout)
         csv_file.writerow(list(column_names))
         with open(json_file_path) as fin:
             for line in fin:
                 line_contents = json.loads(line)
                 csv_file.writerow(get_row(line_contents, column_names))
+
 
 def get_superset_of_column_names_from_file(json_file_path):
     """Read in the json dataset file and return the superset of column names."""
@@ -30,6 +37,7 @@ def get_superset_of_column_names_from_file(json_file_path):
                     set(get_column_names(line_contents).keys())
                     )
     return column_names
+
 
 def get_column_names(line_contents, parent_key=''):
     """Return a list of flattened key names given a dict.
@@ -49,15 +57,16 @@ def get_column_names(line_contents, parent_key=''):
 
     """
     column_names = []
-    for k, v in line_contents.iteritems():
+    for k, v in line_contents.items():
         column_name = "{0}.{1}".format(parent_key, k) if parent_key else k
-        if isinstance(v, collections.MutableMapping):
+        if isinstance(v, collectionsAbc.MutableMapping):
             column_names.extend(
                     get_column_names(v, column_name).items()
                     )
         else:
             column_names.append((column_name, v))
     return dict(column_names)
+
 
 def get_nested_value(d, key):
     """Return a dictionary item given a dictionary `d` and a flattened key from `get_column_names`.
@@ -76,6 +85,8 @@ def get_nested_value(d, key):
     
     """
     if '.' not in key:
+        if d is None:
+            return None
         if key not in d:
             return None
         return d[key]
@@ -85,6 +96,7 @@ def get_nested_value(d, key):
     sub_dict = d[base_key]
     return get_nested_value(sub_dict, sub_key)
 
+
 def get_row(line_contents, column_names):
     """Return a csv compatible row given column names and a dict."""
     row = []
@@ -93,13 +105,14 @@ def get_row(line_contents, column_names):
                         line_contents,
                         column_name,
                         )
-        if isinstance(line_value, unicode):
+        if isinstance(line_value, str):
             row.append('{0}'.format(line_value.encode('utf-8')))
         elif line_value is not None:
             row.append('{0}'.format(line_value))
         else:
             row.append('')
     return row
+
 
 if __name__ == '__main__':
     """Convert a yelp dataset file from json to csv."""
